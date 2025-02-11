3417f66c0d4b2f29b2f3ae99dacad8d47e9d3c7c
Mikael Ryan <mikaelryan97@gmail.com>
2025-02-11 00:31:25 -0300

    Update ferr.py
    updated the tools used to minimize the principal menu code, got it working but theres more to do, need to discover an way to make it create an archive and save it on the PC storage.

--- zzz/utilitarios/ferr.py
+++ zzz/utilitarios/ferr.py
@@ -1,30 +1,40 @@
+import os
+logins = list()
+
+
 def linhas(msg):
     tam = len(msg) + 4
-    print('-'* tam)
+    print('~'*tam)
     print(msg)
-    print('-'*tam)
+    print('~'*tam)
 
 
+def demenu(n):
 
-def dec(n):
-    while True:
-        if n == 1:
-            linhas('OPTION 1')
-            break
-        elif n == 2:
-            linhas('OPTION 2')
-            break
-        
-        else:
-            print('ERROR, try again')
+    if n == 1:
+        os.system('cls')
+        linhas('THOSE ARE THE LOGINS')
+        for c in range(0, len(logins)):
+            print(logins[c])
     
+    
+    elif n == 2:
+        linhas('CREATING AN NEW LOGIN')
+        for c in range(0, 999):
 
-galera = list()
-def rmenu(opc):
-    if opc == 1:
-        linhas('those are the logged persons: ')
-        linhas(galera)
-    elif opc == 2:
-        pessoa = str(input('whats the name? '))
-        galera.append(pessoa)
+            ll = dict() 
+            nick = input('login: ')
+            pas = input('password: ')
+            ll['nick'] = nick
+            ll['password'] = pas
+            logins.append(ll)
+            ll.clear
 
+            dec2 = str(input('more login?[S/N]'))
+            if dec2 in 'Nn':
+                print('ok, ja ane')
+                break
+            elif dec2 in 'Ss':
+                print('')
+            else:
+                print('option invalid, try again')

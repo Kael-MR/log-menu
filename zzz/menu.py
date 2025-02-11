759c02727d077bbd51e4c03030f11396fbbfba3c
Mikael Ryan <mikaelryan97@gmail.com>
2025-02-11 00:28:43 -0300

    Update menu.py
    got it working better, just have to discover a way to make it create an archive with the informations obtained from the code

--- zzz/menu.py
+++ zzz/menu.py
@@ -1,23 +1,24 @@
-from utilitarios import ferr
-galerap = ferr.galera[:]
+from ferramentas import ferr
 
-ferr.linhas('MENU PRINCIPAL')
+ferr.linhas('MENU LOGINS:')
 ferr.linhas('''
-1-see logged people
-2-new login 
-3-exit''')
+1-see logins
+2-add new login
+3-out of code''')
 
 while True:
-    dmenu = int(input('choice? '))
-    if dmenu > 3:
-        print('option not understood, try again')
-    elif dmenu < 1:
-        print('option not understood, try again')
-    elif dmenu == 3:
+    decmenu = int(input('choice?'))
+
+    if decmenu < 1:
+        print('invalid option, try again')
+    elif decmenu > 3:
+        print('invalid option, try again') 
+
+    elif decmenu == 3:
         ferr.linhas('END OF CODE, ja ane')
         break
 
     else:
-        ferr.rmenu(dmenu)
+        ferr.demenu(decmenu)
 
 print('REAL END CODE')
